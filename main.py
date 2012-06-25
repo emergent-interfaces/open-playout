5f10b42ce38a552f3a2c2f7f5ac776eb2f83ca2c
superlou <lousimons@gmail.com>
2012-06-24 20:36:41 -0400

    Show output in drawing area

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import gi
 import sys
 import station
 
-from gi.repository import GObject, Gtk
+from gi.repository import GObject, Gtk, Gdk, GdkX11
 
 class Main:
 	def __init__(self):
@@ -20,6 +20,16 @@ class Main:
 		
 		self.station = station.Station()
 		
+		self.video_window = Gtk.Window()
+		self.drawing_area = Gtk.DrawingArea()
+		self.video_window.add(self.drawing_area)
+		self.drawing_area.show()
+		self.drawing_area.connect("realize", self.on_video_window_realize)
+		self.video_window.show_all()
+	
+	def on_video_window_realize(self, event):
+		self.station.assign_drawing_area(self.drawing_area)
+	
 	def run(self):
 		self.station.run()
 		Gtk.main()
--- station.py
+++ station.py
@@ -1,4 +1,5 @@
 import gi
+import sys
 
 try:
 	gi.require_version('Gst','1.0')
@@ -6,7 +7,7 @@ except ValueError:
 	print 'Could not find required Gstreamer library'
 	sys.exit(1)
 
-from gi.repository import Gst
+from gi.repository import Gst, GstVideo
 print Gst.version_string()
 
 class Station(object):
@@ -17,16 +18,16 @@ class Station(object):
 
 		src = Gst.ElementFactory.make('v4l2src', None)
 		caps_filter = Gst.ElementFactory.make('capsfilter', None)
-		sink = Gst.ElementFactory.make('xvimagesink', None)
+		self.sink = Gst.ElementFactory.make('xvimagesink', None)
 
 		caps_filter.set_property('caps',Gst.caps_from_string("video/x-raw, width=640, height=480"))
 
 		self.pipeline.add(src)
 		self.pipeline.add(caps_filter)
-		self.pipeline.add(sink)
+		self.pipeline.add(self.sink)
 
 		src.link(caps_filter)
-		caps_filter.link(sink)
+		caps_filter.link(self.sink)
 
 		bus = self.pipeline.get_bus()
 		bus.connect('message::eos', self.on_bus_eos)
@@ -36,6 +37,10 @@ class Station(object):
 	def swap(self):
 		# Test hotswap of v4l2src and videotestsrc
 		pass
+		
+	def assign_drawing_area(self, drawing_area):
+		print "DrawingArea ID: " + str(drawing_area.get_window().get_xid())
+		self.sink.set_window_handle(drawing_area.get_window().get_xid())
 
 	def on_bus_message(self, bus, message):
 		pass
