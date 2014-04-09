b559d093e3db3425dbf485eb48536dd485b4397c
Louis Simons <lousimons@gmail.com>
2014-04-08 21:26:34 -0400

    Using log scale for frequencies

--- src/control_panels/audio_test_gen_control_panel.py
+++ src/control_panels/audio_test_gen_control_panel.py
@@ -1,5 +1,6 @@
 from PySide import QtGui, QtCore
 from control_panel import ControlPanel
+import math
 
 
 class AudioTestGenControlPanel(ControlPanel):
@@ -14,18 +15,18 @@ class AudioTestGenControlPanel(ControlPanel):
 
         slider0 = QtGui.QScrollBar()
         slider0.setTracking(True)
-        slider0.setRange(20, 20000)
+        slider0.setRange(0, 1000)
         slider0.setOrientation(QtCore.Qt.Horizontal)
-        slider0.setValue(self.device.freqs.get_value()[0])
+        slider0.setValue(self.freq_to_pos(self.device.freqs.get_value()[0]))
         slider0.valueChanged.connect(self.change_freqs)
         self.vbox.addWidget(slider0)
         self.slider0 = slider0
 
         slider1 = QtGui.QScrollBar()
         slider1.setTracking(True)
-        slider1.setRange(20, 20000)
+        slider1.setRange(0, 1000)
         slider1.setOrientation(QtCore.Qt.Horizontal)
-        slider1.setValue(self.device.freqs.get_value()[1])
+        slider1.setValue(self.freq_to_pos(self.device.freqs.get_value()[1]))
         slider1.valueChanged.connect(self.change_freqs)
         self.vbox.addWidget(slider1)
         self.slider1 = slider1
@@ -33,8 +34,18 @@ class AudioTestGenControlPanel(ControlPanel):
         self.device.freqs.changed.connect(self.freqs_changed)
 
     def change_freqs(self):
-        self.device.freqs.set_value([self.slider0.value(), self.slider1.value()])
+        self.device.freqs.set_value([self.pos_to_freq(self.slider0.value()),
+                                     self.pos_to_freq(self.slider1.value())])
 
     def freqs_changed(self, data):
-        self.slider0.setValue(data[0])
-        self.slider1.setValue(data[1])
+        self.slider0.setValue(self.freq_to_pos(data[0]))
+        self.slider1.setValue(self.freq_to_pos(data[1]))
+
+    a = 303.0
+    b = 0.05
+
+    def freq_to_pos(self, freq):
+        return self.a * math.log10(self.b * freq)
+
+    def pos_to_freq(self, pos):
+        return math.pow(10.0, pos / self.a) / self.b
