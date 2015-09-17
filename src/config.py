cd3a0253811f416a73e49d2a05a9c553b867972b
superlou <lousimons@gmail.com>
2015-09-16 20:26:11 -0400

    Pushing broken DSK, be careful!

--- src/config.py
+++ src/config.py
@@ -19,13 +19,13 @@ import os
 # self.wire('switcher1.prog_out', 'program.in')
 # self.wire('switcher1.prev_out', 'preview.in')
 
-# # Overlay test
-# self.install(VideoTestGen, "video1", (100, 100))
-# d, n = self.install(Dsk, "dsk1", (300, 100))
-# d.file.set_value('../media/lower_third.svg')
-# self.install(Monitor, "screen1", (500, 100))
-# self.wire('video1.out', 'dsk1.in')
-# self.wire('dsk1.out', 'screen1.in')
+# Overlay test
+self.install(VideoTestGen, "video1", (100, 100))
+d, n = self.install(Dsk, "dsk1", (300, 100))
+d.file.set_value('../media/lower_third.svg')
+self.install(Monitor, "screen1", (500, 100))
+self.wire('video1.out', 'dsk1.in')
+self.wire('dsk1.out', 'screen1.in')
 
 # # UStream Test
 # self.install(VideoTestGen, "video1", (100, 100))
@@ -37,12 +37,12 @@ import os
 # self.install(AudioOut, "audioout1", (300, 100))
 # self.wire('testgen1.out', 'audioout1.in')
 
-# Deck Test
-d, n = self.install(Deck, "deck1", (0,0))
-self.install(AudioOut, "audioout1", (300, 100))
-self.install(Monitor, "monitor1", (300,0))
-self.wire('deck1.video_out', 'monitor1.in')
-self.wire('deck1.audio_out', 'audioout1.in')
+# # Deck Test
+# d, n = self.install(Deck, "deck1", (0,0))
+# self.install(AudioOut, "audioout1", (300, 100))
+# self.install(Monitor, "monitor1", (300,0))
+# self.wire('deck1.video_out', 'monitor1.in')
+# self.wire('deck1.audio_out', 'audioout1.in')
 
-media = os.path.abspath(os.path.join(os.getcwd(), '../media'))
-d.file_uri.set_value('file://' + media + '/sintel_trailer-480p.mp4')
+# media = os.path.abspath(os.path.join(os.getcwd(), '../media'))
+# d.file_uri.set_value('file://' + media + '/sintel_trailer-480p.mp4')
--- src/devices/dsk.py
+++ src/devices/dsk.py
@@ -5,6 +5,7 @@ import cairo
 from device import Device
 from observable_variable import ObservableVariable
 from control_panels.dsk_control_panel import DskControlPanel
+import time
 
 try:
     gi.require_version('Gst', '1.0')
@@ -28,38 +29,48 @@ class Dsk(Device):
         self.alpha = ObservableVariable(0)
         self.alpha.changed.connect(self.alpha_changed)
 
-        # self.convert1 = Gst.ElementFactory.make('videoconvert', None)
-        # self.bin.add(self.convert1)
+        # While input convert doesn't seem explicitly necessary, it seems to give
+        # better performance.  Not sure why.
+        self.inputconvert = self.add_element('videoconvert')
+        self.mixer = self.add_element('videomixer')
+        self.outputconvert = self.add_element('videoconvert')
 
-        self.overlay = Gst.ElementFactory.make('gdkpixbufoverlay', None)
-        self.bin.add(self.overlay)
+        self.add_input_video_port_on(self.inputconvert)
+        self.inputconvert.link(self.mixer)
 
-        # self.convert2 = Gst.ElementFactory.make('videoconvert', None)
-        # self.bin.add(self.convert2)
+        self.mixer.link(self.outputconvert)
+        self.add_output_video_port_on(self.outputconvert)
 
-        # self.convert1.link(self.overlay)
-        # self.overlay.link(self.convert2)
-
-        self.add_input_video_port_on(self.overlay)
-        self.add_output_video_port_on(self.overlay)
-
-        #self.overlay.connect("draw", self.draw)
-
-        self.overlay.set_property('overlay_width', self.DEFAULT_VIDEO_WIDTH)
-        self.overlay.set_property('overlay_height', self.DEFAULT_VIDEO_HEIGHT)
+        self.mixer.get_static_pad('sink_0').set_property('zorder', 0)
 
         self.alpha.set_value(1.0)
-        
-    def draw(self, element, ctx, arg2, arg3):
-        print dir(ctx)
-        ctx.move_to(1,1)
 
     def file_changed(self, new_file):
-        try:
-            with open(new_file):
-                self.overlay.set_property('location', new_file)
-        except IOError:
-            print "Overlay file " + new_file + " not found"
+        print "Setting file:", new_file
+        self.filesrc = self.add_element('filesrc')
+        self.gdkdec = self.add_element('gdkpixbufdec')
+        self.convert = self.add_element('videoconvert')
+        self.freeze = self.add_element('imagefreeze')
+        self.caps = self.add_element('capsfilter')
+        self.caps.set_property('caps', Gst.caps_from_string(self.DEFAULT_VIDEO_CAPS))
+
+        self.filesrc.set_property('location', new_file)
+
+        self.filesrc.link(self.gdkdec)
+        self.gdkdec.link(self.convert)
+        self.convert.link(self.freeze)
+        self.freeze.link(self.caps)
+        videomixer_pad = self.mixer.get_request_pad("sink_%u")
+        self.caps.get_static_pad('src').link(videomixer_pad)
+
+        self.filesrc.set_state(Gst.State.PLAYING)
+        self.gdkdec.set_state(Gst.State.PLAYING)
+        self.convert.set_state(Gst.State.PLAYING)
+        self.freeze.set_state(Gst.State.PLAYING)
+        self.caps.set_state(Gst.State.PLAYING)
+
+        videomixer_pad.set_property('zorder', 1)
 
     def alpha_changed(self, alpha):
-        self.overlay.set_property('alpha', alpha)
+        pass
+        # self.overlay.set_property('alpha', alpha)
